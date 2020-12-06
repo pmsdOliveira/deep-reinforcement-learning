f459c1f7ecad894f9568705ec4a1f32fa045d428
pmsdOliveira <46815374+pmsdOliveira@users.noreply.github.com>
2020-12-06 21:09:55 +0000

    Chapter 8 finished

---
+++ Deep Reinforcement Learning Hands-On/Chapter8/lib/models.py
@@ -0,0 +1,68 @@
+import math
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SimpleFFDQN(nn.Module):
+    def __init__(self, obs_len, actions_n):
+        super(SimpleFFDQN, self).__init__()
+
+        self.fc_val = nn.Sequential(
+            nn.Linear(obs_len, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1)
+        )
+
+        self.fc_adv = nn.Sequential(
+            nn.Linear(obs_len, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, actions_n)
+        )
+
+    def forward(self, x):
+        val = self.fc_val(x)
+        adv = self.fc_adv(x)
+        return val + adv - adv.mean()
+
+
+class DQNConv1D(nn.Module):
+    def __init__(self, shape, actions_n):
+        super(DQNConv1D, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv1d(shape[0], 128, 5),
+            nn.ReLU(),
+            nn.Conv1d(128, 128, 5),
+            nn.ReLU()
+        )
+
+        out_size = self._get_conv_out(shape)
+
+        self.fc_val = nn.Sequential(
+            nn.Linear(out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1)
+        )
+
+        self.fc_adv = nn.Sequential(
+            nn.Linear(out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, actions_n)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        conv_out = self.conv(x).view(x.size()[0], -1)
+        val = self.fc_val(conv_out)
+        adv = self.fc_adv(conv_out)
+        return val + adv - adv.mean()
